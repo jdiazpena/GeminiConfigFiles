c339d14af60dc2dd061e7929a4f593bc1160f455
jdiazpena <jdiazpena@gmail.com>
2023-11-15 21:51:58 -0500

    .

--- saidcurvsmaller_fbi_2d/fac_said_gaussian_ramp_2d.py
+++ saidcurvsmaller_fbi_2d/fac_said_gaussian_ramp_2d.py
@@ -2,32 +2,32 @@ import xarray
 import numpy as np
 
 
-def fac_said_gaussian_ramp(E: xarray.Dataset, gridflag: int, flagdip: bool) -> xarray.Dataset:
+def fac_said_gaussian_ramp_2d(E: xarray.Dataset, gridflag: int, flagdip: bool) -> xarray.Dataset:
     """
     for 3D sim, FAC up/down 0.5 degree FWHM
     """
 
-    if E.mlon.size == 1 or E.mlat.size == 1:
-        raise ValueError("for 3D sims only")
+    # if E.mlon.size == 1 or E.mlat.size == 1:
+    #     raise ValueError("for 3D sims only")
 
     # # uniform in longitude
     # shapelon = np.exp(
     #    -((E.mlon - E.mlonmean) ** 2) / 2 / E.mlonsig ** 2
     # )
 
-    # nonuniform in longitude
-    beta=0.7
-    T=1/25
-    f=E.mlon-E.mlonmean
-    shapelon=0*f
+    # # nonuniform in longitude
+    # beta=0.7
+    # T=1/25
+    # f=E.mlon-E.mlonmean
+    # shapelon=0*f
 
-    for i in range(len(f)):
-        if abs(f[i])<(1-beta)/(2*T):
-            shapelon[i]=1
-        elif (1-beta)/(2*T)<abs(f[i]) and abs(f[i])<(1+beta)/(2*T):
-            shapelon[i]=0.5*(1+np.cos( (np.pi*T/beta)*(abs(f[i])-(1-beta)/(2*T)) ))
-        else:
-            shapelon[i]=0
+    # for i in range(len(f)):
+    #     if abs(f[i])<(1-beta)/(2*T):
+    #         shapelon[i]=1
+    #     elif (1-beta)/(2*T)<abs(f[i]) and abs(f[i])<(1+beta)/(2*T):
+    #         shapelon[i]=0.5*(1+np.cos( (np.pi*T/beta)*(abs(f[i])-(1-beta)/(2*T)) ))
+    #     else:
+    #         shapelon[i]=0
     
 
     # nonuniform in latitude
@@ -46,8 +46,8 @@ def fac_said_gaussian_ramp(E: xarray.Dataset, gridflag: int, flagdip: bool) -> x
         k = "Vminx1it" if gridflag == 1 else "Vmaxx1it"
 
         if t>(auxlengthcenter):
-            E[k].loc[E.time[t]] = E.Jtarg * shapelon * shapelat
+            E[k].loc[E.time[t]] = E.Jtarg * shapelat
         else: 
-            E[k].loc[E.time[t]] = E.Jtarg * shapelon * shapelat * (1/(auxlengthcenter) * (t-1))
+            E[k].loc[E.time[t]] = E.Jtarg * shapelat * (1/(auxlengthcenter) * (t-1))
 
     return E
--- saidcurvsmaller_fbi_2d/fac_said_raisedcosine.py
+++ saidcurvsmaller_fbi_2d/fac_said_raisedcosine.py
@@ -2,18 +2,13 @@ import xarray
 import numpy as np
 
 
-def fac_said_gaussian_ramp(E: xarray.Dataset, gridflag: int, flagdip: bool) -> xarray.Dataset:
+def fac_said_raisedcosine(E: xarray.Dataset, gridflag: int, flagdip: bool) -> xarray.Dataset:
     """
     for 3D sim, FAC up/down 0.5 degree FWHM
     """
 
     if E.mlon.size == 1 or E.mlat.size == 1:
         raise ValueError("for 3D sims only")
-
-    # # uniform in longitude
-    # shapelon = np.exp(
-    #    -((E.mlon - E.mlonmean) ** 2) / 2 / E.mlonsig ** 2
-    # )
 
     # nonuniform in longitude
     beta=0.7
@@ -28,26 +23,17 @@ def fac_said_gaussian_ramp(E: xarray.Dataset, gridflag: int, flagdip: bool) -> x
             shapelon[i]=0.5*(1+np.cos( (np.pi*T/beta)*(abs(f[i])-(1-beta)/(2*T)) ))
         else:
             shapelon[i]=0
-    
+
 
     # nonuniform in latitude
     shapelat = -1.0*np.exp(
         -((E.mlat - E.mlatmean - 1.5 * E.mlatsig) ** 2) / 2 / E.mlatsig ** 2
     ) + 1.0*np.exp(-((E.mlat - E.mlatmean + 1.5 * E.mlatsig) ** 2) / 2 / E.mlatsig ** 2)
 
-    aux=E.time[1:]
-    auxlength=aux.shape[0]
-    auxlengthcenter=np.floor(aux.shape[0]/5)
-    auxtime=E.time[int(np.floor(auxlength))]
+    for t in E.time[2:]:
+        E["flagdirich"].loc[t] = 0
 
-
-    for t in range(1,auxlength+1):
-        E["flagdirich"].loc[E.time[t]] = 0
         k = "Vminx1it" if gridflag == 1 else "Vmaxx1it"
-
-        if t>(auxlengthcenter):
-            E[k].loc[E.time[t]] = E.Jtarg * shapelon * shapelat
-        else: 
-            E[k].loc[E.time[t]] = E.Jtarg * shapelon * shapelat * (1/(auxlengthcenter) * (t-1))
+        E[k].loc[t] = E.Jtarg * shapelon * shapelat
 
     return E
